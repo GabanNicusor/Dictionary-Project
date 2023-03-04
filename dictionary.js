247a7aa70e09a1fda11346e470de3015b40dfd9e
GabanNicusor <122691474+GabanNicusor@users.noreply.github.com>
2023-03-04 11:46:50 +0100

    Add files via upload

---
+++ dictionary.js
@@ -0,0 +1,13 @@
+let dictionary = [];
+function data_base_dictionary() {
+    let word = document.getElementById("word_input").value;
+    dictionary.push(word);
+    console.log(dictionary);
+    document.getElementById("word_input").value =" ";
+}
+
+function searchWord() {
+    let word = document.getElementById("search_Word").value;
+     let exist = dictionary.includes(word) ? alert("The word is here :" + " " + word): alert("The word is not in the dictionary!" + " " + ":(");
+     document.getElementById("demo").textContent = exist;   
+}
